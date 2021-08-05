a1e33840d917f01045a7511650fd81464983c95e
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-08-05 18:25:26 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# PiratesInvasionStage-2.5
+created multiple cannonballs.
---
+++ sketch.js
@@ -0,0 +1,80 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+var engine, world, backgroundImg;
+var canvas, angle, tower, ground, cannon;
+var balls = [];
+
+
+function preload() {
+  backgroundImg = loadImage("./assets/background.gif");
+  towerImage = loadImage("./assets/tower.png");
+  
+}
+
+function setup() {
+  canvas = createCanvas(1200,600);
+  engine = Engine.create();
+  world = engine.world;
+  angle = -PI / 4;
+  ground = new Ground(0, height - 1, width * 2, 1);
+  tower = new Tower(150, 350, 160, 310);
+  cannon = new Cannon(180, 110, 100, 50, angle);
+//create a boat object
+  
+
+boat = new Boat(width, height - 100, 200, 200, -100);
+  
+}
+
+function draw() {
+  background(189);
+  image(backgroundImg, 0, 0, width, height);
+
+ 
+
+  Engine.update(engine);
+  ground.display();
+
+ 
+  Matter.Body.setVelocity(boat.body, {
+    x: -0.9,
+    y: 0
+  });
+  boat.display()
+  for (var i = 0; i < balls.length; i++) {
+    showCannonBalls(balls[i], i);
+  }
+//display the boat
+  cannon.display();
+  tower.display();
+
+  
+}
+
+function keyPressed() {
+  if (keyCode === DOWN_ARROW) {
+    var cannonBall = new CannonBall(cannon.x, cannon.y);
+    balls.push(cannonBall);
+  }
+}
+
+//function to show the ball
+function showCannonBalls(ball, index) {
+  ball.display();
+  if (ball.body.position.x >= width || ball.body.position.y >= height - 50) {
+    Matter.World.remove(world, ball.body);
+    balls.splice(index, 1);
+  }
+}
+
+
+
+function keyReleased() {
+  if (keyCode === DOWN_ARROW) { 
+    balls[balls.length - 1].shoot();
+  }
+}
+
+
